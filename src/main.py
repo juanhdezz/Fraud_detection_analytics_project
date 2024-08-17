91606658dbb532796042243c4fd199c94b0b1a3d
juan <jhernandezsanchezagesta@gmail.com>
2024-08-17 13:51:43 +0200

    Añadido proyecto

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+
+# Load data
+df_transactions = pd.read_csv('../data/processed/card_transdata.csv')
+
